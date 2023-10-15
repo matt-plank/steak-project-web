2b5dab75b4c770a03253501f8c28bd383751d08f
matt-plank <matthew_plank@outlook.com>
2023-10-15 20:16:02 +0100

    Fixed typo in website title

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "The Streak Project",
+  title: "The Steak Project",
   description: "Steak with data-driven precision",
 };
 
