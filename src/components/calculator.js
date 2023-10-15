ea03497201dc113675488ac1cb8a38b0a96e3cf3
matt-plank <matthew_plank@outlook.com>
2023-10-15 17:11:27 +0100

    Labelled doneness chart

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -29,12 +29,17 @@ const Calculator = ({
 
         <div className="h-8"></div>
 
-        <div className="grid grid-cols-5 grid-rows-1 gap-2 w-full rounded-md">
-          <div className="bg-rare rounded-sm p-2"></div>
-          <div className="bg-mid-rare rounded-sm p-2"></div>
-          <div className="bg-medium rounded-sm p-2"></div>
-          <div className="bg-mid-well rounded-sm p-2"></div>
-          <div className="bg-well rounded-sm p-2"></div>
+        <div className="grid grid-cols-5 grid-rows-2 gap-2 w-full rounded-md text-white">
+          <p className="text-center text-black">Rare</p>
+          <p className="text-center text-black"></p>
+          <p className="text-center text-black">Medium</p>
+          <p className="text-center text-black"></p>
+          <p className="text-center text-black">Well</p>
+          <div className="bg-rare rounded-sm p-2">00:45</div>
+          <div className="bg-mid-rare rounded-sm p-2">01:00</div>
+          <div className="bg-medium rounded-sm p-2">01:15</div>
+          <div className="bg-mid-well rounded-sm p-2">01:30</div>
+          <div className="bg-well rounded-sm p-2">01:45</div>
         </div>
       </div>
     </>
